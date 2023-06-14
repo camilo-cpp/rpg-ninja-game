50bca8ae2c4c209518b86e3519c89745a1a72a11
Juan Camilo <camil8cho@gmail.com>
2023-06-13 22:12:31 -0500

    Main character logic

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class CharacterAnimation : MonoBehaviour
 {
+    [SerializeField]
+    private string layerIdle;
+    [SerializeField]
+    private string layerWalk;
     private Animator _animator;
     private CharacterMovement _characterMovement;
+    private readonly int directionX = Animator.StringToHash("X");
+    private readonly int directionY = Animator.StringToHash("Y");
 
     private void Awake()
     {
@@ -20,12 +26,35 @@ public class CharacterAnimation : MonoBehaviour
 
     void Update()
     {
-        if (_characterMovement.isMoving == false)
+        UpdateLayer();
+
+        if (!_characterMovement.isMoving)
         {
             return;
         }
 
-        _animator.SetFloat("X", _characterMovement.movementDirection.x);
-        _animator.SetFloat("Y", _characterMovement.movementDirection.y);
+        _animator.SetFloat(directionX, _characterMovement.movementDirection.x);
+        _animator.SetFloat(directionY, _characterMovement.movementDirection.y);
+    }
+
+    private void ActivateLayer(string layerName)
+    {
+        for (int i = 0; i < _animator.layerCount; i++)
+        {
+            _animator.SetLayerWeight(i, 0);
+        }
+
+        _animator.SetLayerWeight(_animator.GetLayerIndex(layerName), 1);
+    }
+
+    private void UpdateLayer()
+    {
+        if (_characterMovement.isMoving)
+        {
+            ActivateLayer(layerWalk);
+        } else
+        {
+            ActivateLayer(layerIdle);
+        }
     }
 }
